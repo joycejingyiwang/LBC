c02f056bf2c581e814a12ae4f9cf30834b3a6466
joycejingyiwang <jw4098@nyu.edu>
2019-04-03 16:59:55 +0800

    04/03
    xoxo

---
+++ demo/Assets/LBC/Script/ClickToTurnOn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToTurnOn : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            this.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+                }
+           
+    }
+}
---
+++ demo/Assets/LBC/Script/lighton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lighton : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            this.GetComponent<Light>().enabled = true;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            this.GetComponent<Light>().enabled = false;
+        }
+    }
+}
