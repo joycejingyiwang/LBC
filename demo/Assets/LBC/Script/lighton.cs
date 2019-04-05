b88241cd7492f374822b4aef83053ee493d60d97
joycejingyiwang <jw4098@nyu.edu>
2019-04-05 21:41:58 +0800

    4/5

--- demo/Assets/LBC/Script/lighton.cs
+++ demo/Assets/LBC/Script/lighton.cs
@@ -6,23 +6,33 @@ public class lighton : MonoBehaviour
 {
     public GameObject obj1;
     public GameObject obj2;
+    public GameObject anchor;
     public float d;
+    private float angle;
+    private Vector3 origin;
+    private Vector3 direction;
 
     // Update is called once per frame
     void Update()
     {
         d = Vector3.Distance(obj1.transform.position, obj2.transform.position);
+        origin = anchor.transform.position- obj2.transform.position ;
+        direction = obj1.transform.position - obj2.transform.position;
+        angle = Vector3.Angle(origin, direction);
 
-        if (Input.GetKeyDown(KeyCode.O)&& d<20)
+        if (d < 20 && angle < 30)
         {
-            if( this.GetComponent<Light>().enabled) {
-                this.GetComponent<Light>().enabled = false;
-            }
-            else
-            {
-                this.GetComponent<Light>().enabled = true;
+           if (Input.GetMouseButtonDown(0))
+                {
+
+                if (this.GetComponent<Light>().enabled) {
+                    this.GetComponent<Light>().enabled = false;
+                }
+                else
+                {
+                    this.GetComponent<Light>().enabled = true;
+                }
             }
         }
-      
     }
 }
